21b0d9b5fd704b33668b551757213e66748e4405
Baker <baker@shamlan.sa>
2022-03-22 14:43:29 +0300

    Update About.vue

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -18,6 +18,13 @@
             >Back Home</router-link
           >
         </div>
+        <div class="inline-flex rounded-md shadow">
+          <router-link
+            to="/contact"
+            class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out hover:bg-indigo-500 focus:outline-none"
+            >Next Page</router-link
+          >
+        </div>
         <ButtonRepo />
       </div>
     </div>
